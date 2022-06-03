103b60e557ffddc78c70799f20075ac2a2739e6b
Azeez Samad <seshp@ymail.com>
2022-06-03 18:06:19 +0100

    add layout for homepage and registration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import Header from './components/Header'
-import './default.scss'
-import Homepage from './pages/Homepage'
+import "./default.scss";
+import Homepage from "./pages/Homepage";
+import Registration from "./pages/Registration";
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import HomepageLayout from "./layout/HomepageLayout";
 
 function App() {
   return (
     <div className="App">
-     <Header />
-     <div className="main">
-     <Homepage />
-     </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <HomepageLayout>
+              <Homepage />
+            </HomepageLayout>
+          }
+        />
+
+        <Route
+          path="/registration"
+          element={
+            <MainLayout>
+              <Registration />{" "}
+            </MainLayout>
+          }
+        />
+      </Routes>
     </div>
   );
 }
