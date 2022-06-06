aa3dfbc6001dd2fd35b79488e0f6641ee31c97ac
Azeez Samad <seshp@ymail.com>
2022-06-06 15:58:08 +0100

    add login page

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignIn from '../../components/SignIn';
+import './styles.scss'
+
+const Login = () => {
+  return (
+    <div>
+      {/* <h1>Login</h1> */}
+      <SignIn />
+    </div>
+  );
+}
+
+export default Login;
